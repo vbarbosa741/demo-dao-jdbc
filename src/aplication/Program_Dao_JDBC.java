7efa41f92c238720833e682e280b84dea5ca0727
Valdemir Barbosa <vbarbosa741@gmail.com>
2020-05-29 13:33:26 -0300

    FindById implementation

--- src/aplication/Program_Dao_JDBC.java
+++ src/aplication/Program_Dao_JDBC.java
@@ -1,8 +1,5 @@
 package aplication;
 
-import java.util.Date;
-
-import model.entities.Department;
 import model.entities.Seller;
 import modelDao.DaoFactory;
 import modelDao.SellerDao;
@@ -11,10 +8,11 @@ public class Program_Dao_JDBC {
 
 	public static void main(String[] args) {
 	
-		Department obj = new Department(1 , "Rafael");
-		Seller seller = new  Seller(2, "Bob", "bob@gmail", new Date(), 25000.0, obj);
+		
 		
 		SellerDao sellerDao = new DaoFactory().createSellerDao();
+		Seller seller = sellerDao.finfById(3);
+		
 		System.out.println(seller);
 
 	}
--- src/modelDao/DaoFactory.java
+++ src/modelDao/DaoFactory.java
@@ -1,11 +1,12 @@
 package modelDao;
 
+import db.DB;
 import model.Dao.Impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao () {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 
 }
