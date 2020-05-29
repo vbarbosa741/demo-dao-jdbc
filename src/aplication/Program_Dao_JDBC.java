d5d5fdcaedd836da2ce3132ded3ed045ea0a7ae1
Valdemir Barbosa <vbarbosa741@gmail.com>
2020-05-29 19:25:25 -0300

    Impementation findByDEpartment

--- src/aplication/Program_Dao_JDBC.java
+++ src/aplication/Program_Dao_JDBC.java
@@ -1,5 +1,9 @@
 package aplication;
 
+import java.util.List;
+
+import model.Dao.Impl.SellerDaoJDBC;
+import model.entities.Department;
 import model.entities.Seller;
 import modelDao.DaoFactory;
 import modelDao.SellerDao;
@@ -13,9 +17,18 @@ public class Program_Dao_JDBC {
 		SellerDao sellerDao = new DaoFactory().createSellerDao();
 		
 		System.out.println("====Teste 01 : FindById =====");
-		Seller seller = sellerDao.finfById(3);
-		
+		Seller seller = sellerDao.finfById(3);	
 		System.out.println(seller);
+		
+		System.out.println("\n====Teste 02 : FindByDepartment=====");
+		
+		Department department = new Department(2, null);
+		List<Seller> list = sellerDao.findByDepartment(department);
+		for(Seller obj : list) {
+			
+			System.out.println(obj);
+		}
+		
 
 	}
 
--- src/modelDao/SellerDao.java
+++ src/modelDao/SellerDao.java
@@ -2,6 +2,7 @@ package modelDao;
 
 import java.util.List;
 
+import model.entities.Department;
 import model.entities.Seller;
 
 public interface SellerDao {
@@ -11,6 +12,7 @@ public interface SellerDao {
 	void deleteById(Integer id);
 	Seller finfById(Integer id);
 	List<Seller> findAll();
+	List<Seller> findByDepartment (Department department);
 
 }
 
