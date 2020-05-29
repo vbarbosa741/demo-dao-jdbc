5662e4bf402f000212d56ebd3cc36c12886dd702
Valdemir Barbosa <vbarbosa741@gmail.com>
2020-05-28 21:01:32 -0300

    Department entity class

---
+++ src/aplication/Program_Dao_JDBC.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Program_Dao_JDBC {
+
+	public static void main(String[] args) {
+	
+		Department obj = new Department(1 , "Rafael");
+		System.out.println(obj);
+
+	}
+
+}
