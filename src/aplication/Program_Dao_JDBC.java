51b534c059e6da075117e1fd27770ced74cb0883
Valdemir Barbosa <vbarbosa741@gmail.com>
2020-05-28 21:19:44 -0300

    Seller entity class

--- src/aplication/Program_Dao_JDBC.java
+++ src/aplication/Program_Dao_JDBC.java
@@ -1,13 +1,17 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program_Dao_JDBC {
 
 	public static void main(String[] args) {
 	
 		Department obj = new Department(1 , "Rafael");
-		System.out.println(obj);
+		Seller seller = new  Seller(2, "Bob", "bob@gmail", new Date(), 25000.0, obj);
+		System.out.println(seller);
 
 	}
 
